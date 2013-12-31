475d2e67b2edfe462eb0089c733a550c5268260b
Mike Wyatt <wyatt.mike@gmail.com>
2013-12-31 11:30:45 -0400

    fix broken specs

--- spec/jenkins_tracker/tracker_client_spec.rb
+++ spec/jenkins_tracker/tracker_client_spec.rb
@@ -6,12 +6,17 @@ describe JenkinsTracker::TrackerClient do
     it 'returns an instance of RestClient::Resource' do
       tracker_client = described_class.new(:token => 'xxx')
       expect(tracker_client.connection).to be_an_instance_of(RestClient::Resource)
-      expect(tracker_client.connection.to_s).to eq('http://www.pivotaltracker.com/services/v3')
+      expect(tracker_client.connection.to_s).to eq('https://www.pivotaltracker.com/services/v5')
     end
 
-    it 'does not use an SSL connection by default' do
+    #it 'does not use an SSL connection by default' do
+      #tracker_client = described_class.new(:token => 'xxx')
+      #expect(tracker_client.connection.to_s).to eq('http://www.pivotaltracker.com/services/v5')
+    #end
+
+    it 'does use an SSL connection by default' do
       tracker_client = described_class.new(:token => 'xxx')
-      expect(tracker_client.connection.to_s).to eq('http://www.pivotaltracker.com/services/v3')
+      expect(tracker_client.connection.to_s).to eq('https://www.pivotaltracker.com/services/v5')
     end
   end
 
