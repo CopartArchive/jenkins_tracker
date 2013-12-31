8464c6e9bfcc223e71bafab7abb3cf3505232e1d
Mike Wyatt <wyatt.mike@gmail.com>
2013-12-31 11:30:39 -0400

    fix whitespace

--- lib/jenkins_tracker/tracker_client.rb
+++ lib/jenkins_tracker/tracker_client.rb
@@ -27,7 +27,7 @@ module JenkinsTracker
 
     def add_note_to_story(project_id, story_id, note)
       begin
-	puts "Adding comment to story ##{story_id}..."
+        puts "Adding comment to story ##{story_id}..."
         connection["projects/#{project_id}/stories/#{story_id}/comments"].post({ 'text' => note }.to_json)
       rescue => e
         # if the post fails for whatever reason (e.g. invalid story id etc), just ignore it
@@ -37,7 +37,7 @@ module JenkinsTracker
 
     def deliver_story(project_id, story_id)
       begin
-	puts "Changing ownership to #{@acceptor_name} and story status (##{story_id}) to 'Delivered'"
+        puts "Changing ownership to #{@acceptor_name} and story status (##{story_id}) to 'Delivered'"
         connection["projects/#{project_id}/stories/#{story_id}"].put({ 'current_state' => 'delivered', 'owned_by_id' => @acceptor_id }.to_json)
       rescue => e
         # if the post fails for whatever reason (e.g. invalid story id etc), just ignore it
